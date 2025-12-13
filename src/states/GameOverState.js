4dff925c60e57cd6dc0afde02bc555793d30a7c3
Cody-P-6260059 <6260059@johnabbottcollege.net>
2025-12-13 12:43:06 -0500

    Implemented panels for whent he player dies or reached the end. They now get the option to cotinue playing or go back to the menu and select another level.

--- src/states/GameOverState.js
+++ src/states/GameOverState.js
@@ -1,7 +1,108 @@
 import State from "../../lib/State.js";
+import GameStateName from "../enums/GameStateName.js";
+import ImageName from "../enums/ImageName.js";
+import Colour from "../enums/Colour.js";
+import Panel from "../user-interface/elements/Panel.js";
+import Selection from "../user-interface/elements/Selection.js";
+import {
+    CANVAS_HEIGHT,
+    CANVAS_WIDTH,
+    context,
+    images,
+    stateMachine,
+    timer,
+} from "../globals.js";
 
 export default class GameOverState extends State {
-	constructor() {
-		super();
-	}
+    /**
+     * Game Over screen with options to retry or return to menu
+     */
+    constructor() {
+        super();
+    }
+
+    enter() {
+        // Create the panel for the menu
+        const panelWidth = 300;
+        const panelHeight = 220;
+        const panelX = (CANVAS_WIDTH - panelWidth) / 2;
+        const panelY = (CANVAS_HEIGHT - panelHeight) / 2;
+
+        this.panel = new Panel(panelX, panelY, panelWidth, panelHeight, {
+            borderColour: Colour.Crimson,
+            panelColour: Colour.White,
+        });
+
+        // Create the selection menu with options
+        const selectionX = panelX + 20;
+        const selectionY = panelY + 80;
+        const selectionWidth = panelWidth - 40;
+        const selectionHeight = panelHeight - 100;
+
+        this.selection = new Selection(
+            selectionX,
+            selectionY,
+            selectionWidth,
+            selectionHeight,
+            [
+                {
+                    text: "Try Again",
+                    onSelect: () => {
+                        this.tryAgain();
+                    },
+                },
+                {
+                    text: "Back to Level Select",
+                    onSelect: () => {
+                        this.backToMenu();
+                    },
+                },
+            ]
+        );
+    }
+
+    update(dt) {
+        timer.update(dt);
+        this.selection.update();
+    }
+
+    render() {
+        // Render background image
+        images.render(ImageName.Background, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+        // Render the panel
+        this.panel.render();
+
+        // Render "GAME OVER" title
+        context.save();
+        context.font = "40px Zelda";
+        context.fillStyle = "crimson";
+        context.textAlign = "center";
+        context.textBaseline = "middle";
+        context.fillText("GAME OVER", CANVAS_WIDTH / 2, CANVAS_HEIGHT / 2 - 60);
+        context.restore();
+
+        // Render the selection menu
+        this.selection.render();
+    }
+
+    /**
+     * Restart the current level
+     */
+    tryAgain() {
+        stateMachine.change(GameStateName.Transition, {
+            fromState: this,
+            toStateName: GameStateName.Play,
+        });
+    }
+
+    /**
+     * Go back to the level selection menu
+     */
+    backToMenu() {
+        stateMachine.change(GameStateName.Transition, {
+            fromState: this,
+            toStateName: GameStateName.HomeScreen,
+        });
+    }
 }
